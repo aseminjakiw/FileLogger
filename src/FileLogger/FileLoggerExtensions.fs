74b8c61fc0978f9e0adc98785332efbcf1401b49
Alexander Seminjakiw <45207231+aseminjakiw@users.noreply.github.com>
2024-01-07 11:28:51 +0100

    Add playground projects for F# and C#

---
+++ src/FileLogger/FileLoggerExtensions.fs
@@ -0,0 +1,10 @@
+﻿namespace FileLogger
+
+open System.Runtime.CompilerServices
+open Microsoft.Extensions.Logging
+
+
+[<Extension>]
+type FileLoggerExtensions =
+    [<Extension>]
+    static member AddFile(builder: ILoggingBuilder) : ILoggingBuilder = failwith "not implemented"
--- src/Playground/Program.fs
+++ src/Playground/Program.fs
@@ -1,2 +1,30 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open Microsoft.Extensions.Hosting
+open Microsoft.Extensions.Logging
+open Microsoft.Extensions.DependencyInjection
+
+open FileLogger
+
+type ExampleClass() =
+    class
+    end
+
+let executeLogger (logger: ILogger<ExampleClass>) = logger.LogInformation "Hello from F#"
+
+[<EntryPoint>]
+let main args =
+    task {
+        let builder = Host.CreateApplicationBuilder(args)
+
+        let _ = builder.Logging.AddFile()
+        let host = builder.Build()
+        do! host.StartAsync()
+
+        let logger = host.Services.GetRequiredService<ILogger<ExampleClass>>()
+        do executeLogger logger
+
+        do! host.StopAsync()
+
+        return 0
+    }
+    |> Async.AwaitTask
+    |> Async.RunSynchronously
